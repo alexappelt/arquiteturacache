5bfae1e422f7ff2e7ae122dfe4247984991c4a8f
guibrust <gbrust15@gmail.com>
2019-11-19 20:33:00 -0300

    struct

--- geradorbinario.cpp
+++ geradorbinario.cpp
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void menu(){
+	printf(" 1- Ler Memoria\n 2- Escrever memoria\n 3- Estatisticas\n");
+	printf(" Entre com a opcao: ");
+}
+
+void menu2(){
+	printf("\n 1- Numero de Acessos\n 2- Numero de Acertos\n 3- Numero de Faltas\n");
+	printf(" 4- Numero de Leituras\n 5- Numero de Escritas\n 6- Numero de Acertos na Leitura\n");
+	printf(" 7- Numero de Acertos na Escrita\n 8- Numero de Faltas na Leitura\n 9- Numero de Faltas na Escrita\n");
+	printf(" Entre com a opcao: ");
+}
+
+typedef struct{
+	char dado;
+	int bloco;
+	char endereco[8];
+}memPrinc;
+
+typedef struct{
+	char celulaA, celulaB, celulaC,CelulaD;
+	char  rotulo;
+	int quadro;
+}memCach;
+
 int main()
 {
    int num;
