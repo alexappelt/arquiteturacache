4a83f901eb1f18e44e69e3893057c184500e33ea
guibrust <gbrust15@gmail.com>
2019-11-30 09:35:14 -0300

    testes

--- trabMem.cpp
+++ trabMem.cpp
@@ -108,7 +108,7 @@ void lerMem(memPrinc *p, memCach *c, estatisticas *es){
 	cin>>endere;
 	es->nacessos++;
 	es->leitura++;
-	for(int i=0;i<6;i++){
+	for(int i=0;i<6;i++){//calcula endereco lido
 		if(i==0&&endere[i]=='1')//qual o num. do rotulo
 			blocoNovo=blocoNovo+2;
 		if(i==1&&endere[i]=='1')
@@ -133,8 +133,6 @@ void lerMem(memPrinc *p, memCach *c, estatisticas *es){
 		if(i==1&&c[blcach].rotulo[i]=='1')
 			blocoAtual=blocoAtual+1;
 	}
-	cout<<"novo rotulo "<<blocoNovo<<endl;
-	cout<<"rotulo atual "<<blocoAtual<<endl;
 	
 	if(blocoAtual!=blocoNovo){
 		es->faltas++;
@@ -142,7 +140,6 @@ void lerMem(memPrinc *p, memCach *c, estatisticas *es){
 		for(int i=0;i<256;i++){
 			if(strncmp(endere,p[i].endereco,8)==0){//pesquisa qual o bloco na principal
 				blprinc=p[i].bloco;
-				cout<<"bl princi"<<blprinc<<endl;
 			}
 		}
 		for(int i=0;i<256;i++){//grava informaçoes na cache
@@ -164,8 +161,85 @@ void lerMem(memPrinc *p, memCach *c, estatisticas *es){
 		es->acerleitura++;
 	}
 	system("pause");
-
 }
+
+void escreverCache(memPrinc *p, memCach *c, estatisticas *es){
+	char endereco[8];
+	string novo;
+	int novoRot=0,quadro=0,celula=0;
+	int celulaP=0,rotuloAtual=0;
+	bool a=false;
+		
+	cout<<"Entre com o endereco para ser escrito:[        ]\b\b\b\b\b\b\b\b\b";
+	cin>>endereco;
+	cout<<"Entre com o dado para ser escrito: ";
+	cin>>novo;
+	
+	for(int i=0;i<8;i++){
+		if(i==0&&endereco[i]=='1'){
+			novoRot=novoRot+2;
+			celulaP=celulaP+128;}
+		else if(i==1&&endereco[i]=='1'){
+			novoRot=novoRot+1;
+			celulaP=celulaP+64;}
+		else if(i==2&&endereco[i]=='1'){
+			quadro=quadro+8;
+			celulaP=celulaP+32;}
+		else if(i==3&&endereco[i]=='1'){
+			quadro=quadro+4;
+			celulaP=celulaP+16;}
+		else if(i==4&&endereco[i]=='1'){
+			quadro=quadro+2;
+			celulaP=celulaP+8;}
+		else if(i==5&&endereco[i]=='1'){
+			quadro=quadro+1;
+			celulaP=celulaP+4;}
+		else if(i==6&&endereco[i]=='1'){
+			celula=celula+2;
+			celulaP=celulaP+2;}
+		else if(i==7&&endereco[i]=='1'){
+			celula=celula+1;
+			celulaP=celulaP+1;}
+	}
+	p[celulaP].dado=novo;//muda principal
+	for(int i=0;i<2;i++){			
+		if(i==0&&c[quadro].rotulo[i]=='1')//qual o num. do rotulo atual
+			rotuloAtual=rotuloAtual+2;
+		if(i==0&&c[quadro].rotulo[i]=='2'){
+			rotuloAtual=rotuloAtual+4;
+			i++;}
+		if(i==1&&c[quadro].rotulo[i]=='1')
+			rotuloAtual=rotuloAtual+1;
+	}
+	
+	if(rotuloAtual!=novoRot){
+		for(int i=0;i<256;i++){//grava informaçoes na cache
+			if(strncmp(endereco,p[i].endereco,8)==0){//procura bloco na mem.principal
+				c[quadro].celulaA=p[i].dado;
+				c[quadro].celulaB=p[i+1].dado;
+				c[quadro].celulaC=p[i+2].dado;
+				c[quadro].celulaD=p[i+3].dado;
+				c[quadro].rotulo[0]=endereco[0];
+				c[quadro].rotulo[1]=endereco[1];
+				a=true;
+			}
+			if(a)
+				break;
+		}
+	}
+	else{
+		if(celula==0)
+			c[quadro].celulaA=novo;
+		else if(celula==1)
+			c[quadro].celulaB=novo;
+		else if(celula==2)
+			c[quadro].celulaC=novo;
+		else if(celula==3)
+			c[quadro].celulaD=novo;
+	}
+	system("pause");	
+}
+
 void mostraCache(memCach *c){
 	printf(" Rotulo  Quadro Celula0 Celula1 Celula2 Celula3\n");
 	for(int i=0;i<16;i++){
@@ -203,12 +277,14 @@ int main(){
 		system("cls");
 		mostraCache(memC);
 		menu();
+		
+		
 		scanf("%d", &n);
 		if(n==1){//ler memoria
 			lerMem(memP, memC,&est);
 		}
 		else if(n==2){//escrever memoria
-			
+			escreverCache(memP, memC,&est);
 		}
 		else if(n==3){//statisticas
 			menu2(&est);			
